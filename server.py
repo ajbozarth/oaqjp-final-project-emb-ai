bef4cd75f1845c4506c3a79712ac771899038880
Alex Bozarth <ajbozart@us.ibm.com>
2024-11-07 20:57:37 -0500

    Task 8

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''' Emotion Detector server
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,11 +7,13 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    ''' call emotion_detector and return a formatted string
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     result = emotion_detector(text_to_analyze)
 
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
     return (f"For the given statement, the system response is 'anger': {result['anger']}, "
@@ -18,6 +22,8 @@ def emotion_detect():
 
 @app.route("/")
 def render_index():
+    ''' render the index page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
