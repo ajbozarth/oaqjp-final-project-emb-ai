f8a1343bf671f18dc5d8601b33da947122f20280
Alex Bozarth <ajbozart@us.ibm.com>
2024-11-07 20:51:50 -0500

    Task 7

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ def emotion_detect():
 
     result = emotion_detector(text_to_analyze)
 
+    if result['dominant_emotion'] == None:
+        return "Invalid text! Please try again!"
+
     return (f"For the given statement, the system response is 'anger': {result['anger']}, "
         f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and "
         f"'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}.")
